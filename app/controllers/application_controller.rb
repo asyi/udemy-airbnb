611e8a490ccfa45cb0bc330bae696d0b9ce804c5
Althea Yi <altheaserena.yi@gmail.com>
2018-04-06 21:05:20 -0400

    Creates first and lastname fields and devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_paramaters, if: :devise_controller?
+
+  protected
+  def configure_permitted_paramaters
+    devise_paramater_sanitizer.permit(:sign_up, keys: [:firstname, :lastname])
+    devise_paramater_sanitizer.permit(:account_update, keys: [:firstname, :lastname])
+  end
+  
 end
